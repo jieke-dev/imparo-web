72941af936f76cd8b65d785e4c3f7e138a10e581
Jie Ke Huang <jieke.huang316@gmail.com>
2026-09-20 11:10:43 +0200

    somma tra due numeri

---
+++ prova2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Ciao!";
+    int a, b;
+
+    cin >> a >> b;
+    cout << "La somma di " << a << " e " << b << " è: " << a + b;
+    
+    return 0;
+}
